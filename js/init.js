e766d331fda9b055044ac469a89c0bb4f77ed0cc
yuanwei-rom <2931724757@qq.com>
2017-07-11 11:36:12 +0800

    Frist release

---
+++ js/init.js
@@ -0,0 +1,15 @@
+Materialize.toast('欢迎您来到原味官方!', 1500);
+	
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+	
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+
+
+
+
